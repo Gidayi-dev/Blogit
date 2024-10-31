82b61924a83238e0d18004e040da5947d51dcc6a
Gidayi Millyannah <millyannahi@gmail.com>
2024-10-31 16:36:37 +0300

    hero

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,14 +5,6 @@ function Header() {
     return(
         <div className="header-section">
             <div className="logo"><img src={Logo} alt="Logo" /><br /><h2>Blogit</h2></div>
-            <div className="nav-links">
-                <ul>
-                    <li><a href="#" className="links">Home</a></li>
-                    <li><a href="#" className="links">About</a></li>
-                    <li><a href="#" className="links">Blogs</a></li>
-                    <li><a href="#" className="links">Contact</a></li>
-                </ul>
-            </div>
             <div className="login">
                 <button className="button">Sign in</button>
                 <button className="button">Get Started</button>
---
+++ src/components/hero.jsx
@@ -0,0 +1,14 @@
+import "./hero.css";
+
+function Hero() {
+    return (
+        <div className="hero-section">
+            <div className="hero-text"><h1>Stories, Skills & Creative Journeys</h1>
+            <p>Welcome to a space where creativity, growth, and discovery come together! Dive into a variety of topics, from personal development and productivity tips to creative pursuits and storytelling. Join the journey and find inspiration for your own path</p>
+            <button className="explore">Explore</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
