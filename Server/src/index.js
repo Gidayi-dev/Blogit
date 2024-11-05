e954243c0096ed83c0ec58eb1083fd1e3b482b0a
Gidayi Millyannah <millyannahi@gmail.com>
2024-11-05 16:02:01 +0300

    login route

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 
@@ -82,15 +83,17 @@ app.post("/auth/SignIn", async (req, res) => {
         .status(401)
         .json({ message: "Wrong email address or password" });
     }
-    //successfully signed in
-    res
-      .status(200)
-      .json({
-        message: "Login successfully",
-        user: { id: user.id, emailAddress: user.emailAddress },
-      });
+    //successfully signed in, so generate a token, save the id in there
+    const token = jwt.sign(user, process.env.JWT_SECRET);
+
+    // res
+    //   .status(200)
+    //   .json({
+    //     message: "Login successfully",
+    //     user: { id: user.id, emailAddress: user.emailAddress },
+    //   });
     //authentication failure if user does not exist
-    res.send("logging the user in");
+    res.status(200).cookie("authToken", token, { httpOnly: true }).json(user);
   } catch (e) {
     console.error("SignIn error:", e);
     res.status(500).json({ message: "Something went wrong please try again" });
